e956c4cd94c904b02d14aba9e0740980bb02fced
actyvystom <thomas.goerldt@actyvyst.com>
2022-11-03 10:52:56 +0100

    update complex state structures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import "./App.css";
+import { useState } from "react";
+import { useImmer } from "use-immer";
 
 export default function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [items, setItems] = useState([1, 2]);
+  const [player, setPlayer] = useImmer({
+    name: "Horst",
+    position: { x: 3, y: 4 }
+  });
+
+  const updatePlayer = () => {
+    setPlayer((draft) => {
+      draft.position.x = 7;
+      draft.position.y = 9;
+      draft.name = "Wursthorst";
+    });
+  };
+
+  const handleItemUpdate = () => {
+    setItems([...items, 3, 4]);
+  };
+
+  //   const changePosition = () => {
+  //     setPosition({ ...position, x: (position.x += 5), y: 6 });
+  //   };
+
   return (
     <div className="App">
       <h1>React useState and Events - Advanced</h1>
       <h2>Updating nested state structures</h2>
-      {/* <button onClick={handlePlayerUpdate}>update position</button>
-      <p>{`${player.position.x} ${player.name}`}</p> */}
+      {/* <button onClick={changePosition}>update </button> */}
+      <button onClick={updatePlayer}>update </button>
+      <p>{`X: ${position.x} Y: ${position.y}`}</p>
+      <p>{`${items}`}</p>
+      <p>{`${JSON.parse(JSON.stringify(player))}`}</p>
     </div>
   );
 }
